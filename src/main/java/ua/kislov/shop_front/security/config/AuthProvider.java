798d2b1f3e84c1dab0ab0c09919de0df4797a216
Andrey <neznajkaandrej99@gmail.com>
2023-04-07 22:08:41 +0300

    Solve problem with logout and authenticate. And add style for html.

--- src/main/java/ua/kislov/shop_front/security/config/AuthProvider.java
+++ src/main/java/ua/kislov/shop_front/security/config/AuthProvider.java
@@ -1,7 +1,9 @@
 package ua.kislov.shop_front.security.config;
 
+import feign.RetryableException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -26,12 +28,19 @@ public class AuthProvider implements AuthenticationProvider {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
         //получаем по username со стороннего сервиса (аутентификация и регистрация)
-        ClientDetails details = (ClientDetails) securityClientDetailsService.loadUserByUsername(username);
+        ClientDetails details;
+        try{
+             details = (ClientDetails) securityClientDetailsService.loadUserByUsername(username);
+            System.out.println(details);
+        }catch (RetryableException e){
+            throw new AuthenticationServiceException("Exception service is not available");
+        }
+
         String password = authentication.getCredentials().toString();
 
         if(!password.equals(details.getPassword()))
             throw new BadCredentialsException("Password is not correct!");
-        return new UsernamePasswordAuthenticationToken(details, password, Collections.emptyList());
+        return new UsernamePasswordAuthenticationToken(details, password, details.getAuthorities());
     }
 
     @Override
